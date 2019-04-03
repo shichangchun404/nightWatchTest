892a23b7c1d1aa0836cb03ab410c1c3be8f3daf2
shizhangchun <shichangchun@jd.com>
2019-04-03 19:09:19 +0800

    test runner

--- test/e2e/specs/index_specs.js
+++ test/e2e/specs/index_specs.js
@@ -13,20 +13,22 @@
 //       .end();
 //   }
 // };
-const testUrl = 'https://www.baidu.com/'
-describe('test for baidu.com', () => {
-  // 页面正常渲染
-  it('render page content success', function (browser) {
-    browser
-      .url(testUrl)
-      .waitForElementVisible('body', 3000)
-      .setValue('input[id=kw]', 'nightwatch')
-      .waitForElementVisible('input[type=submit]', 1000)
-      .click('input[type=submit]')
-      .pause(1000)
-      .waitForElementVisible('div[class=s_tab_inner]', 1000)
-      .assert.containsText('.s_tab_inner', '网页')
-      .end();
-  })
 
-})
+
+// const testUrl = 'https://www.baidu.com/'
+// describe('test for baidu.com', () => {
+//   // 页面正常渲染
+//   it('render page content success', function (browser) {
+//     browser
+//       .url(testUrl)
+//       .waitForElementVisible('body', 3000)
+//       .setValue('input[id=kw]', 'nightwatch')
+//       .waitForElementVisible('input[type=submit]', 1000)
+//       .click('input[type=submit]')
+//       .pause(1000)
+//       .waitForElementVisible('div[class=s_tab_inner]', 1000)
+//       .assert.containsText('.s_tab_inner', '网页')
+//       .end();
+//   })
+
+// })
